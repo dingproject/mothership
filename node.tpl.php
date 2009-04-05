2af6aac3a06f72fc9270f89519ff6995a7801548
mortendk <drupal@morten.dk>
2009-04-05 15:59:37 +0000

    cleaned up tpl files

--- node.tpl.php
+++ node.tpl.php
@@ -12,26 +12,16 @@
 	<?php if ($node->picture) { ;?>
     <?php print theme('imagecache', 'preset_namespace', $node->picture, $alt, $title, $attributes); ?>
 	<?php } ?>
+	
+	<?php print theme('username', $node); ?>
 
-		<span class="user-name">
-			<?php print theme('username', $node); ?>
-		</span>	
+	<?php print format_date($node->created, 'custom', "j F Y") ?> 
 
-		<span class="date">
-			<?php print format_date($node->created, 'custom', "j F Y") ?> 
-		</span>	
-
-	</div>
-
-	<div class="content clearfix">
-		<?php  print $content;?>	
-		<a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print t('read more') ?> - </a>
-	</div>
+	<?php  print $content;?>	
+	<a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print t('read more') ?>
 
   <?php if ($links){ ?>
-    <div class="links">
-      <?php print $links; ?>
-    </div>
+    <?php print $links; ?>
   <?php }; ?>
 	  
 </div>
@@ -39,7 +29,6 @@
 <?php }else{ 
 //Content
 ?>
-
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes ?> clearfix">
 	<h1><?php print $title;?></h1>
 		
@@ -67,7 +56,5 @@
 	<?php if ($links){ ?>
     <?php  print $links; ?>
 	<?php } ?>
-
 </div>
-
 <?php } ?>
